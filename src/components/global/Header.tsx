08013f2f2a791aae351f7429dc8d1215a9bdde8e
anshuman-coder <anshurockingboy23@gmail.com>
2024-03-14 00:23:53 +0530

    header component base setup

---
+++ src/components/global/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='flex h-20 w-full flex-row items-center justify-around border-b-[1px] border-gray-300 bg-white'>
+      This is Header
+    </header>
+  )
+}
+
+export default Header
--- src/components/global/PageBox.tsx
+++ src/components/global/PageBox.tsx
@@ -2,17 +2,16 @@ import React from 'react'
 import type { ReactNode, FC } from 'react'
 import clsx from 'clsx'
 import Loader from './Loader'
+import Header from './Header'
 
 interface PageBoxProps {
   isLoading?: boolean
   children: ReactNode
-  header?: ReactNode
 }
 
 const PageBox: FC<PageBoxProps> = ({
   isLoading = false,
   children,
-  header,
 }) => {
   return (
     <main className={ clsx(
@@ -22,9 +21,7 @@ const PageBox: FC<PageBoxProps> = ({
       {
         !isLoading ? (
           <>
-            <header className='flex w-full h-20'>
-              { header }
-            </header>
+            <Header />
             <section className='grid grid-cols-12'>
               {
                 children
