bdd99112f830bcbe8cbe682a2c1edeaa87c67f24
anshuman-singh23 <anshuman.singh@purebillion.tech>
2024-03-24 15:43:00 +0530

    Global Input component

--- src/components/dashboard/MainSection.tsx
+++ src/components/dashboard/MainSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import type { FC } from 'react'
 import { Search } from 'lucide-react'
-import { Button, DropDown, Loader } from '~/components/global'
+import { Button, DropDown, Loader, Input } from '~/components/global'
 import type { Option } from '~/components/global/DropDown'
 
 const OPTIONS: Option[] =[
@@ -21,21 +21,12 @@ const MainSection: FC = () => {
     <main className='col-span-8 border-r border-gray-300 px-20'>
       <div className='w-full flex flex-col space-y-4 py-4'>
         <div className='w-full flex items-center space-x-4'>
-          <label
-             htmlFor='search'
-             className='relative w-full rounded-3xl border border-gray-800 pl-4'
-          >
-            <div className='absolute left-2 flex h-full items-center '>
-              <Search className='w-6 h-6' />
-            </div>
-            <input
-              type='text'
-              name='search'
-              id='search'
-              className='w-full rounded-3xl py-1 px-4 pl-7 text-sm outline-none placeholder:text-xs placeholder:text-gray-300'
-              placeholder='Search...'
-            />
-          </label>
+          <Input
+            id='search'
+            startIcon={<Search className='w-6 h-6' />}
+            placeholder='Search...'
+            type='text'
+          />
           <div className='flex w-full items-center justify-end gap-x-4'>
             <p className='text-sm'>My topics:</p>
             <div className='flex items-center space-x-2'>
---
+++ src/components/global/Input.tsx
@@ -0,0 +1,63 @@
+// import { Search } from 'lucide-react'
+import React from 'react'
+import type { ReactNode, FC, InputHTMLAttributes } from 'react'
+import clsx from 'clsx'
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  fullWidth?: boolean
+  startIcon?: ReactNode
+  containerClass?: string
+  endIcon?: ReactNode
+  className?: string
+}
+
+const Input: FC<InputProps> = (props) => {
+  const {
+    id,
+    containerClass = '',
+    fullWidth = true,
+    startIcon,
+    endIcon,
+    className = '',
+    ...otherProps
+  } = props
+  return (
+    <label
+        htmlFor={id}
+        className={
+          clsx(
+            'relative rounded-3xl border border-gray-800 pl-4',
+            containerClass,
+            fullWidth ? 'w-full' : 'w-fit',
+          )
+        }
+    >
+      {
+        startIcon && (
+          <div className='absolute top-0 left-2 flex h-full items-center '>
+            {startIcon}
+          </div>
+        )
+      }
+      <input
+        id={id}
+        className={
+          clsx(
+            'w-full rounded-3xl py-1 px-4 pl-7 text-sm outline-none placeholder:text-xs placeholder:text-gray-300',
+            className,
+          )
+        }
+        {...otherProps}
+      />
+      {
+        endIcon && (
+          <div className='absolute top-0 right-2 flex h-full items-center '>
+            {endIcon}
+          </div>
+        )
+      }
+    </label>
+  )
+}
+
+export default Input
