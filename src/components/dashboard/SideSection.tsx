ee6abf7ef445db81a6d8cd3bce0fc0d670f3340d
anshuman-coder <anshurockingboy23@gmail.com>
2024-03-25 21:58:33 +0530

    resolving the changes

--- src/components/dashboard/SideSection.tsx
+++ src/components/dashboard/SideSection.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import type { FC } from 'react'
-import Image from 'next/image'
-import { format } from 'date-fns'
 import { Card as PostCard } from '~/components/post'
+import { Card as UserCard } from '~/components/user'
 
 const SideSection: FC = () => {
   return (
@@ -16,7 +15,7 @@ const SideSection: FC = () => {
           {
             Array.from({ length: 4 })
               .map((user, i) => (
-                <PostCard key={i} />
+                <UserCard key={i} />
               ))
           }
         </div>
@@ -31,33 +30,7 @@ const SideSection: FC = () => {
             Array.from({ length: 4 })
               .map((post, i) => (
                 // this has to be next link
-                <div key={i} className='group flex items-center space-x-4'>
-                  <div className='aspect-square h-full w-2/5 rounded-xl bg-gray-300' />
-                  <div className='flex w-3/5 flex-col space-y-2'>
-                    <div className='text-sm font-semibold decoration-indigo-600 group-hover:underline'>
-                      Hello World
-                    </div>
-                    <div className='truncate text-xs'>
-                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry #&apos s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    </div>
-                    <div className='flex w-full items-center space-x-2'>
-                      <div className='relative h-8 w-8 rounded-full bg-gray-300'>
-                        <Image
-                          src={'/avatar.png'}
-                          fill
-                          alt='user-image'
-                          className='rounded-full'
-                        />
-                      </div>
-                      <div className='text-xs'>
-                        Anshuman Singh &#x2022;
-                      </div>
-                      <div className='text-xs'>
-                        {format(new Date(), 'dd/MM/yyyy')}
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <PostCard key={i} />
               ))
           }
         </div>
