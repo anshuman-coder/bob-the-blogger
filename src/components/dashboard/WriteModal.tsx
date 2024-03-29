f0f2af486c16cc3e7315918b6ad661073aee368b
anshuman-coder <anshurockingboy23@gmail.com>
2024-03-30 00:45:03 +0530

    create or write post modal complete with inputs not with tags

--- src/components/dashboard/WriteModal.tsx
+++ src/components/dashboard/WriteModal.tsx
@@ -1,16 +1,122 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import type { FC, PropsWithChildren } from 'react'
-import { Modal } from '~/components/global'
+import { Modal, Input, Button } from '~/components/global'
+import { Controller, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import dynamic from 'next/dynamic'
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false })
+import 'react-quill/dist/quill.snow.css'
 
 interface WriteModalProps extends PropsWithChildren {
   isOpen: boolean
   setIsOpen: (bool: boolean) => void
 }
 
+export const writeFormSchema = z.object({
+  title: z.string().max(20),
+  description: z.string().min(60).max(200),
+  text: z.string().min(100).optional(),
+  html: z.string().min(100),
+})
+
+export type WriteFormType = z.infer<typeof writeFormSchema>
+
 const WriteModal: FC<WriteModalProps> = ({ isOpen, setIsOpen }) => {
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<WriteFormType>({
+    resolver: zodResolver(writeFormSchema)
+  })
+
+  const onSubmit = useCallback((data: WriteFormType) => {
+    console.log(data)
+  }, [])
+
+  const handleClose = useCallback(() => {
+    reset()
+    setIsOpen(false)
+  }, [reset, setIsOpen])
+
   return (
-    <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} title='Write a post'>
-      Anshuman
+    <Modal isOpen={isOpen} onClose={handleClose} title='Write a post'>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className='w-full flex flex-col items-center justify-center space-y-2 pb-20 overflow-y-auto'>
+          <Controller
+            name='title'
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <Input
+                type='text'
+                containerClass='h-full w-full rounded-xl border border-gray-300 p-4 outline-none focus:border-gray-600'
+                className='pl-1 placeholder:text-lg text-xl'
+                placeholder='Title of the blog'
+                value={value}
+                onChange={onChange}
+              />
+            )}
+          />
+          <p className='w-full pb-2 text-left text-sm text-red-500'>
+            {errors?.title?.message}
+          </p>
+          <Controller
+            control={control}
+            name='description'
+            render={({ field: { value, onChange } }) => (
+              <Input
+                type='text'
+                containerClass='h-full w-full rounded-xl border border-gray-300 p-4 outline-none focus:border-gray-600'
+                className='pl-1 placeholder:text-base'
+                placeholder='Short Description about the blog'
+                value={value}
+                onChange={onChange}
+              />
+            )}
+          />
+          <p className='w-full pb-2 text-left text-sm text-red-500'>
+            {errors?.description?.message}
+          </p>
+          <Controller
+            name='html'
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <div className='w-full'>
+                <ReactQuill
+                  theme='snow'
+                  placeholder='Write the blog body here'
+                  className='h-32'
+                  value={value}
+                  onChange={onChange}
+                />
+              </div>
+            )}
+          />
+        </div>
+        <div className='sticky w-full bottom-0 left-0 flex justify-end gap-x-4 p-4 bg-white border-t border-gray-300'>
+          <Button
+            variant='secondary'
+            type='submit'
+            className='py-1.5 px-4'
+            circled
+          >
+            Publish
+          </Button>
+          <Button
+            className='py-1.5 px-4'
+            circled
+            onClick={handleClose}
+            type='button'
+          >
+            Cancel
+          </Button>
+        </div>
+      </form>
     </Modal>
   )
 }
